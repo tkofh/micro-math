329eb9cd1e5d32cc5057ae5d492b5df0796f5c8e
Tim Morris <timthewebguy@gmail.com>
2022-12-12 20:48:00 -0500

    chore: switch to pnpm

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,14 +1,4 @@
-const { getPackages } = require('@commitlint/config-lerna-scopes').utils;
-
 module.exports = {
-  extends: [
-    '@commitlint/config-lerna-scopes',
-    '@commitlint/config-conventional'
-  ],
-  rules: {
-    'scope-enum': ctx => getPackages(ctx).then(packages => [2, 'always', [
-      ...packages,
-      'release'
-    ]])
-  }
-};
+  extends: ['@commitlint/config-conventional'],
+  plugins: ['commitlint-plugin-workspace-scopes'],
+}
